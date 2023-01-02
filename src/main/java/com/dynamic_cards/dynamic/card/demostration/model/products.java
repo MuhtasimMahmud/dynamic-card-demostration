872789c3dd762dd34637228460bb51b418cb55b0
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2023-01-02 20:05:22 +0600

    updated

--- src/main/java/com/dynamic_cards/dynamic/card/demostration/model/products.java
+++ src/main/java/com/dynamic_cards/dynamic/card/demostration/model/products.java
@@ -16,6 +16,47 @@ public class products {
     private String bio;
 
 
-    
+    public products(){
 
+    }
+
+    public products(int id, String name, String email, String bio) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.bio = bio;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
 }
