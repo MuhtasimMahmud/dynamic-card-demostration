dc7bb6d01cb343559d90205bd3aaf2d608a12c97
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2023-01-03 06:32:51 +0600

    updated

--- src/main/java/com/dynamic_cards/dynamic/card/demostration/repositories/productsRepository.java
+++ src/main/java/com/dynamic_cards/dynamic/card/demostration/repositories/productsRepository.java
@@ -1,5 +1,13 @@
 package com.dynamic_cards.dynamic.card.demostration.repositories;
 
-public interface productsRepository {
+
+import com.dynamic_cards.dynamic.card.demostration.model.products;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface productsRepository extends JpaRepository<products, Integer> {
+
+    public List<products> findAll();
 
 }
